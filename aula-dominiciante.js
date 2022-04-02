051caf8c85bf2263b2bd3c4b422ce8ee87c34a7f
Thiago David <thiagodavidpd@gmail.com>
2022-04-02 19:10:17 -0300

    Star with DOM on JS exercises

---
+++ aula-dominiciante.js
@@ -0,0 +1,18 @@
+// Retorne o url da página atual utilizando o objeto window
+
+console.log(window.location.href);
+
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+
+var primeiroAtivo = document.querySelector('.ativo');
+console.log(primeiroAtivo);
+
+// Retorne a idioma do navegador
+
+var idioma = window.navigator.language;
+
+// Retorne a largura da janela
+
+var largura = screen.width;
+console.log(largura);
